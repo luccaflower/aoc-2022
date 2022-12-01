2483cd7bf13393f51f75e1e9ecf4231421528ead
lucca <rkaasgaard@gmail.com>
2022-12-01 21:09:00 +0100

    methods and eta expansion

--- src/main/scala/day_one/CaloriesCalculator.scala
+++ src/main/scala/day_one/CaloriesCalculator.scala
@@ -7,7 +7,7 @@ object Calories {
   def mostCaloriesOf: List[String] => Int = l => totalCaloriesOfEach(l).max
   
 
-  def totalCaloriesOfEach: List[String] => List[Int] = l =>
+  def totalCaloriesOfEach(l: List[String]): List[Int] =
     l.foldLeft(List(0))((a, b) => b match {
       case s if s.isEmpty() => a.+:(0)
       case s => {
@@ -16,23 +16,24 @@ object Calories {
       } 
     })
   
+  def topThree(l: List[Int]): List[Int] = l.sortWith(_ > _).take(3)
 
-  def topThree: List[Int] => List[Int] = l => l.sortWith(_ > _).take(3)
-
-  def parse: String => List[String] = s => s.lines().toScala(List).map(_.trim())
+  def parse(s: String): List[String] = s.lines().toScala(List).map(_.trim())
 }
 
 object MainPartOne {
   import Calories._
-  def main(args: Array[String]): Unit = 
-    println(mostCaloriesOf(parse(Source.fromFile(args(0)).mkString)))
+  def main(args: Array[String]): Unit = {
+    val lines = Source.fromFile(args(0)).mkString
+    println((parse _ andThen mostCaloriesOf)(lines))
+  }
 }
 
 object MainPartTwo {
   import Calories._
   def main(args: Array[String]): Unit = {
     val lines = Source.fromFile(args(0)).mkString
-    println((parse andThen totalCaloriesOfEach andThen topThree)(lines).sum)
+    println((parse _ andThen totalCaloriesOfEach andThen topThree)(lines).sum)
   }
 
 }
