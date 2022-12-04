15f6805bb2f0002d48400898687c1c4d2affbf23
lucca <rkaasgaard@gmail.com>
2022-12-04 11:45:23 +0100

    day three

---
+++ src/main/scala/day_three/Rucksacks.scala
@@ -0,0 +1,40 @@
+package rucksacks
+import scala.io.Source
+object Rucksacks {
+  def halvesOf(s: String): (String, String) = s.splitAt(s.length/2)
+  def inBoth(c: (String, String)): Char = c._1.find(c._2.contains(_)).get
+  def inEach(l: Seq[String]): Char = 
+    l(0).find(c => l(1).contains(c) && l(2).contains(c)).get
+}
+
+case class Item(c: Char) {
+  def priority: Int =
+    if (c.isUpper) c.toInt-38
+    else c.toInt-96
+}
+
+object MainPartOne {
+  import Rucksacks._
+  def main(args: Array[String]) = {
+    val input = Source.fromFile(args(0))
+    val result = input.getLines()
+      .map(halvesOf(_))
+      .map(inBoth(_))
+      .map(Item(_).priority)
+      .sum
+    println(result)
+  }
+}
+
+object MainPartTwo {
+  import Rucksacks._
+  def main(args: Array[String]) = {
+    val input = Source.fromFile(args(0))
+    val result = input.getLines()
+      .grouped(3)
+      .map(inEach(_))
+      .map(Item(_).priority)
+      .sum
+    println(result)
+  }
+}
---
+++ src/test/scala/RucksacksTest.scala
@@ -0,0 +1,50 @@
+import org.scalatest.funsuite._
+import org.scalatest.matchers.should._
+
+class RucksacksTest extends AnyFunSuite with Matchers {
+  val input = 
+    """vJrwpWtwJgWrhcsFMMfFFhFp
+      |jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+      |PmmdzqPrVvPwwTWBwg
+      |wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+      |ttgJtRGJQctTZtZT
+      |CrZsJsPPZsGzwwsLwLmpwMDw""".stripMargin
+  import rucksacks.Rucksacks._
+  import rucksacks.Item
+  test("split at length/2 splits in half") {
+    val firstLine = input.linesIterator
+      .map(halvesOf(_))
+      .toList(0)
+    firstLine._1.length should equal(firstLine._2.length)
+  }
+
+  test("items have priority") {
+    Item('a').priority should equal(1)
+    Item('z').priority should equal(26)
+    Item('A').priority should equal(27)
+    Item('Z').priority should equal(52)
+  }
+
+  test("for each rucksack, one item appears in both halves") {
+    input.linesIterator
+      .map(halvesOf(_))
+      .map(inBoth(_))
+      .toList(0) should equal('p')
+  }
+
+  test("a rucksack has the priority of the item that appears in both compartments") {
+    input.linesIterator
+      .map(halvesOf(_))
+      .map(inBoth(_))
+      .map(Item(_).priority)
+      .sum should equal(157)
+  }
+
+  test("for each three rucksacks, and item appears in each") {
+    input.linesIterator
+      .grouped(3)
+      .map(inEach(_))
+      .map(Item(_).priority)
+      .sum should equal(70)
+  }
+}
